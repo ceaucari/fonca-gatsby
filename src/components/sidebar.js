b3f77818c5dbd4eea35308f1ca2691e084808a89
Cesar Castillo <ceaucari@gmail.com>
2020-10-27 20:36:16 -0600

    Setup for github pages

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -16,17 +16,17 @@ export default function Sidebar() {
     <div className="sidebar" style={styles.sidebar}>
       <ul>
         <li>
-          <Link style={styles.link} to="/">
+          <Link style={styles.link} to="/fonca-gatsby/">
             Home
           </Link>
         </li>
         <li>
-          <Link style={styles.link} to="/series/1">
+          <Link style={styles.link} to="/fonca-gatsby/series/1">
             Series 1
           </Link>
         </li>
         <li>
-          <Link style={styles.link} to="/series/2">
+          <Link style={styles.link} to="/fonca-gatsby/series/2">
             Series 2
           </Link>
         </li>
