d76cb2e297a95e2a2a167c78d958fd429897e853
Cesar Castillo <ceaucari@gmail.com>
2020-10-31 01:32:56 -0600

    Hopefully fixing images not found issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@
 const path = require("path")
 
 module.exports = {
-  pathPrefix: `/fonca-gatsby`,
+  pathPrefix: "/fonca-gatsby",
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
-        path: path.join(__dirname, `src`, `images`),
+        name: "images",
+        path: path.join(__dirname, "src", "images"),
       },
     },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
   ],
 }
